36060c398d8387c67c3fc979cde9744ac1b72509
Enid Maharjan <enidmaharjan@yahoo.com>
2026-07-14 23:25:32 -0500

    Create GET products endpoint

---
+++ src/main/java/com/example/product_api/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.example.product_api.controller;
+
+import com.example.product_api.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String getProducts() {
+        return "Products API is working";
+    }
+}
